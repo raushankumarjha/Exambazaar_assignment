16a49bc44d3a75a02696eed3836fe21460451474
Raushan Kumar Jha <16uec081@lnmiit.ac.in>
2020-06-21 12:06:36 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
-import QuestionPagePaper from './components/QuestionPagePaper';
+//import QuestionPagePaper from './components/QuestionPagePaper';
 
 ReactDOM.render(<App/>, document.getElementById('root'));
 registerServiceWorker();
